d0d5bc5097ebd3c7eb883792369f669b0a2d11f0
Omar-Khaled2198 <omar21621@gmail.com>
2019-07-14 02:02:07 +0200

    change profile routes names

--- server/routes/employee/profile.route.js
+++ server/routes/employee/profile.route.js
@@ -4,8 +4,8 @@ var router = express.Router();
 var ProfileController = require("../../controllers/employee/profile.controller");
 var Upload = require("../../middleware/file.midldeware");
 
-router.post('/',ProfileController.CreateProfile);
-router.get('/',ProfileController.GetProfile);
+router.post('/profile',ProfileController.CreateProfile);
+router.get('/profile',ProfileController.GetProfile);
 router.post('/profile_picture',Upload.single("profile_picture"),ProfileController.UploadProfileImage);
 
 module.exports = router;
