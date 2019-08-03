72726176437fc52a5aace5da1cbb5a3fce0dc69c
Omar-Khaled2198 <omar21621@gmail.com>
2019-08-03 03:10:51 +0200

    add abort vacation service

--- client/src/services/vacation.service.js
+++ client/src/services/vacation.service.js
@@ -23,10 +23,24 @@ export const RequestVacationService = async function(title,description,from,to){
 
 }
 
-export const GetVacations = async function () {
+export const GetVacationsService = async function () {
 
     try {
         return await axios.get(`${constants.apiUrl}/employee/${global.profile_id}/vacations`, {
+            headers: {
+                'x-access-token': global.token
+            }
+        });
+
+    } catch (error) {
+        return error.response;
+    }
+}
+
+export const AbortVacationService = async function (vacation_id){
+
+    try {
+        return await axios.put(`${constants.apiUrl}/employee/${global.profile_id}/vacations/${vacation_id}`, {
             headers: {
                 'x-access-token': global.token
             }
